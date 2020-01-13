7e1ba4ae6211c259a8a79794edd28d2549e79927
OrganicNeuralNetwork11 <56148816+OrganicNeuralNetwork11@users.noreply.github.com>
2020-01-13 22:00:50 +1100

    Barebones concept
    Not the actual bot. But a barebones visualization of what we plan for it to do

---
+++ barebones.py
@@ -0,0 +1,13 @@
+
+complimentContainer = []
+
+photoBank = []
+
+complimentGathering = input()
+complimentBank.append(complimentGathering)
+
+print('Thank you for helping brighten the world!')
+print(complimentBank)
+
+print('Here is a gift in return')
+# Send photo fom photo bank
